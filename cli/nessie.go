f5b223082c4c16889bf2a47e3b70e4aa3079929e
ibradypod <ibradypod@gmail.com>
2020-12-13 16:25:07 +0800

    support apiKeys

--- cli/nessie.go
+++ cli/nessie.go
@@ -3,19 +3,20 @@ package main
 
 import (
 	"flag"
-	"github.com/attwad/nessie"
+	"github.com/ibradypod/nessie"
 	"io/ioutil"
 	"log"
 	"strings"
 	"time"
 )
 
-var apiURL, username, password, fingerprints string
+var apiURL, username, password, fingerprints, apiKeys string
 
 func init() {
 	flag.StringVar(&apiURL, "api_url", "", "")
 	flag.StringVar(&username, "username", "", "Username to login with, in production read that from a file, do not set from the command line or it will end up in your history.")
 	flag.StringVar(&password, "password", "", "Password that matches the provided username, in production read that from a file, do not set from the command line or it will end up in your history.")
+	flag.StringVar(&apiKeys, "api_keys", "", "These keys are generated per account through session: keys or users: keys and can be used to authenticate without creating a session. Like: 'accessKey={accessKey}; secretKey={secretKey};'")
 	flag.StringVar(&fingerprints, "fingerprints", "", "Comma-separated list of SPKI Fingerprints for the Nessus server using SHA-256 encoded in base64.")
 	flag.Parse()
 }
@@ -31,13 +32,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	if err := nessus.Login(username, password); err != nil {
-		log.Println(err)
-		return
+	if apiKeys != "" {
+		nessus.SetApiKeys(apiKeys)
+	} else {
+		if err := nessus.Login(username, password); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("Logged-in")
+		defer nessus.Logout()
 	}
-	log.Println("Logged-in")
-	defer nessus.Logout()
 
 	var scanID int64 = 13
 	// We only care about the last scan, so no use for the scan UUID here.
